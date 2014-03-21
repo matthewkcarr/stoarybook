73413265fcc4d484090623ff533ebda859c8be3a
Matthew Carr <bikokid@gmail.com>
2014-03-21 16:58:25 -0700

    added about controller pluse devise routes

--- stoarybook/config/routes.rb
+++ stoarybook/config/routes.rb
@@ -1,10 +1,18 @@
 Stoarybook::Application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'devise/sessions#new'
+
+  devise_for :users
+  root 'about#index'
+  #root 'devise/sessions#new'
+  #devise_scope :user do
+  #  get "/" => "devise/sessions/new"
+  #end
+
+  #root :action => 'new', :controller => 'devise/sessions'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
