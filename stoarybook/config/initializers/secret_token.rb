a76e049feb568f54abd074a58f84c4e9fc4a07f8
Matthew Carr <bikokid@gmail.com>
2014-03-21 16:35:23 -0700

    added rails ember stoarybook app

---
+++ stoarybook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stoarybook::Application.config.secret_key_base = 'd1f69fb36f3de43e68a7cd1abd7ed887513c0584a24b7c05e7613078c1ae9bbf62516ff790d3f023fcacdca434b3cabbee02bc0c8e6910072e8011df8de3849b'
