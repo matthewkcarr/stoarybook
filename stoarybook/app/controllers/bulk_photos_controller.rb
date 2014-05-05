da834ae88ea1cf6dec5191a2dbba825ad6022129
Matthew Carr <bikokid@gmail.com>
2014-05-05 15:06:07 -0700

    updated styling to fit with a more modern look

--- stoarybook/app/controllers/bulk_photos_controller.rb
+++ stoarybook/app/controllers/bulk_photos_controller.rb
@@ -9,27 +9,41 @@ class BulkPhotosController < ApplicationController
   end
 
   def create
-    if BookPhoto.create( photo_params )
+    @book_photo = BookPhoto.create( photo_params )
+    if @book_photo 
       respond_to do |format|
         format.html {
           redirect_to :action => :index
         }
-        format.json { render :status => :created, :template => nil}
+        #format.json { render :json => {:message => 'File uploaded sucessfully'}, :status => :success }
+        format.json { render json: {files: [to_jq_upload(@book_photo)]}, status: :created, location: @book_photo }
       end
     else
       respond_to do |format|
         format.html {
           render :template => :new
         }
-        format.json { render :status => :unprocessable_entity, :template => nil}
+        #format.json { render :json => {}, :status => :unprocessable_entity}
+        format.json { render json: @book_photo.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
+
   def photo_params
     #params[:book_photo][:picture] ||= []
     params.require(:book_photo).permit(:picture, :picture => [])
+  end
+
+  def to_jq_upload(book_photo)
+    return {
+      "name" => book_photo.picture_file_name,
+      "size" => book_photo.picture_file_size,
+      "url" => book_photo.picture.url(:original),
+      "delete_url" => book_photo_path(book_photo),
+      "delete_type" => "DELETE"
+    }
   end
 
 end
